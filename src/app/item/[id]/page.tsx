be83c1b808fb03445d3c743245d5c5ace2cd15ef
Nikita Grebnev <n.a.grebnev@gmail.com>
2025-01-25 23:55:12 +0500

    type fix

--- src/app/item/[id]/page.tsx
+++ src/app/item/[id]/page.tsx
@@ -2,7 +2,11 @@ import InteractiveView from "@/app/_components/InteractiveView";
 import EXAMPLE_ITEMS from "@/items";
 import { notFound } from "next/navigation";
 
-export default async function Item({ params }: { params: { id: string } }) {
+export default async function Item({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   const item = EXAMPLE_ITEMS.find((item) => item.id === id);
   if (!item) notFound();
